4b22b4549448c9c1b0b1d0aa8f2e8cc0ab48bf7f
stefanieZhao77 <stefaniezhao414@gmail.com>
2019-11-12 20:59:15 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # picture_wall
+
+
+This is a 3D picture wall for Angel Wang 
+
+为我鸥姐打call！
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  },
+};
